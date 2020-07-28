0046d04d5c8d092e356ba3b8f4def6fab652d607
Maricaya <915270549@qq.com>
2020-07-28 21:59:49 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Monica></Monica>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Monica from './components/Monica.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Monica
+  }
+}
+</script>
