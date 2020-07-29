706ed957185349ea35bf8b2b3d0f1ee78839053d
Maricaya <915270549@qq.com>
2020-07-29 13:56:30 +0800

    更新 topnav 样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -26,5 +26,21 @@
 </script>
 
 <style lang="scss" scoped>
-
+	.topnav {
+		background: pink;
+		display: flex;
+		padding: 16px;
+		> .logo {
+			max-width: 6em;
+			margin-right: auto;
+		}
+		> .menu {
+			display: flex;
+			white-space: nowrap;
+			flex-wrap: nowrap;
+			> li {
+				margin: 0 1em;
+			}
+		}
+	}
 </style>
