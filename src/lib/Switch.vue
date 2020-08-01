2b798a712c6e0231733d730e507e572a3686cd82
Maricaya <915270549@qq.com>
2020-08-01 16:45:09 +0800

    switch 可以切换了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,12 +1,20 @@
 <template>
-	<button>
+	<button @click="toggle" :class="{checked}">
 		<span></span>
 	</button>
 </template>
 
 <script lang="ts">
+  import {ref} from "vue";
+
   export default {
-    props: {},
+    setup() {
+      const checked = ref(false);
+      const toggle = () => {
+				checked.value =!checked.value;
+      };
+      return {checked, toggle};
+    }
   };
 </script>
 
@@ -17,10 +25,11 @@
 		height: $h;
 		width: $h*2;
 		border: none;
-		background: blue;
+		background: grey;
 		border-radius: $h/2;
 		position: relative;
 	}
+
 	span {
 		position: absolute;
 		top: 2px;
@@ -29,5 +38,12 @@
 		width: $h2;
 		background: #fff;
 		border-radius: $h2/2;
+	}
+	button.checked {
+		background: blue;
+	}
+	button.checked > span {
+		left: calc(100% - #{$h2} - 2px);
+
 	}
 </style>
