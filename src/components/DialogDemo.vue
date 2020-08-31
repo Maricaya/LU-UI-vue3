2907b114db76a0b2894aba736c878d173d8a5e0e
Maricaya <915270549@qq.com>
2020-08-31 12:29:30 +0800

    创建 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,16 @@
 <template>
-	<div>dialog</div>
+	<div>dialog 示例</div>
+	<h1>示例 1</h1>
+	<Dialog></Dialog>
 </template>
 
 <script>
+	import Dialog from '../lib/Dialog.vue'
   export default {
-    name: "DialogDemo"
+    name: "DialogDemo",
+	  components: {
+      Dialog
+	  }
   }
 </script>
 
