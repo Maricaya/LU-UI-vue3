d56d5af16def1c486c3f23556f4d1f6cfb6f7dbf
Maricaya <915270549@qq.com>
2020-08-31 13:11:06 +0800

    Dialog visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,26 @@
 <template>
-	<div>dialog 示例</div>
-	<h1>示例 1</h1>
-	<Dialog></Dialog>
+		<div>dialog 示例</div>
+		<h1>示例 1</h1>
+		<Button @click="toggle">toggle</Button>
+		<Dialog :visible="x"></Dialog>
 </template>
 
 <script>
-	import Dialog from '../lib/Dialog.vue'
+  import Dialog from '../lib/Dialog.vue'
+  import Button from '../lib/Button.vue'
+  import {ref} from 'vue'
   export default {
-    name: "DialogDemo",
 	  components: {
-      Dialog
+      Dialog,
+		  Button
+	  },
+	  setup () {
+	    const x = ref(false)
+		  const toggle = () => {x.value = !x.value}
+		  return {
+	      x,
+			  toggle
+		  }
 	  }
   }
 </script>
