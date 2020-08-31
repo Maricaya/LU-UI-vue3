ad46585a884556c1c3f0e88bb71c5502fd427f39
Maricaya <915270549@qq.com>
2020-08-31 14:20:08 +0800

    使用 openDialog 打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,7 +1,18 @@
 <template>
 		<div>dialog 示例</div>
-		<h1>示例 1</h1>
+		<h2>示例 1</h2>
 		<Button @click="toggle">toggle</Button>
+		<Dialog v-model:visible="x" :close-on-click-overlay="false" :ok="f1" :cancel="f2">
+			<template v-slot:title>
+				<h1>标题</h1>
+			</template>
+			<template v-slot:content>
+				<strong>hi</strong>
+				<div>hi</div>
+			</template>
+		</Dialog>
+		<h2>示例 2</h2>
+		<Button @click="showDialog">toggle</Button>
 		<Dialog v-model:visible="x" :close-on-click-overlay="false" :ok="f1" :cancel="f2">
 			<template v-slot:title>
 				<h1>标题</h1>
@@ -17,6 +28,8 @@
   import Dialog from '../lib/Dialog.vue'
   import Button from '../lib/Button.vue'
   import {ref} from 'vue'
+  import {openDialog} from "../lib/openDialog"
+  
   export default {
 	  components: {
       Dialog,
@@ -33,10 +46,19 @@
 	      return true
       };
 		  const toggle = () => {x.value = !x.value}
+		  const showDialog = () => {
+		    openDialog({
+			    title:'标题',
+			    content: '内容',
+          ok() {console.log('ok')},
+          cancel() {console.log('cancel')}
+		    })
+		  }
 		  return {
 	      x,
 			  toggle,
-			  f1, f2
+			  f1, f2,
+        showDialog
 		  }
 	  }
   }
