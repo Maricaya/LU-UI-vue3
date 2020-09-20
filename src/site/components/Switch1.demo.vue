2bb5c9f6d6d859724f53a76d9dc35cf988725689
Maricaya <915270549@qq.com>
2020-09-20 13:19:19 +0800

    refactor: Extract the code copied by the user

---
+++ src/site/components/Switch1.demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <Switch v-model:value="checked"></Switch>
+</template>
+
+<script lang="ts">
+import {Switch} from "../../lib";
+import {ref} from "vue";
+
+export default {
+  components: {Switch},
+  setup() {
+    const checked = ref(true);
+    return {checked};
+  }
+}
+</script>
